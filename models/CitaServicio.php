e0bd9722a80a2e6991ac855a6dcce04a9d312d10
Borja Florin Fernandez <borjasff3@gmail.com>
2023-08-02 22:54:41 +0200

    actualizacion de CitaServicios

--- models/CitaServicio.php
+++ models/CitaServicio.php
@@ -3,7 +3,7 @@ namespace Model;
 
 class CitaServicio extends ActiveRecord{
         //Base de datos
-        protected static $tabla = 'citasServicios';
+        protected static $tabla = 'citasservicios';
         //es importante asegurar que estos datos estan sanitizados, vienen de la funcion sanitizarAtributo
         protected static $columnasDB = [ 'id', 'citaId','servicioId'];
 
