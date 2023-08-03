a42bc14a7e5a0e0842a62198ebf7262e33d65e22
Borja Florin Fernandez <borjasff3@gmail.com>
2023-08-03 17:38:58 +0200

    actualizacion de formulario

--- views/servicios/formulario.php
+++ views/servicios/formulario.php
@@ -2,7 +2,7 @@
     <label for="nombre">Nombre</label>
     <input type="text" id="nombre" name="nombre" placeholder="Nombre Servicio" value="<?php echo $servicio->nombre; ?>">
 </div>
-</br>
+
 <div class="campo">
     <label for="precio">Precio</label>
     <input type="number" id="precio" name="precio" placeholder="Precio Servicio" value="<?php echo $servicio->precio; ?>">
